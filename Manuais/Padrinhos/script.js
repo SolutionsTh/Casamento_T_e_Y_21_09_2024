0e73fbab3c685d2d838e1155887f19381f2d4a97
SolutionsTh <133886425+SolutionsTh@users.noreply.github.com>
2024-01-23 14:55:10 -0300

    Add files via upload

---
+++ Manuais/Padrinhos/script.js
@@ -0,0 +1,26 @@
+function proximaImgIframeImg(){
+    if(document.getElementById("imagem").className.slice(-1) < 9){
+        document.getElementById("imagem").src = "../../Img/Manual/Padrinhos_Completo/" + (parseFloat(document.getElementById("imagem").className.slice(-1)) + 1) + ".png";
+
+        document.getElementById("imagem").className = "imagem imagem" + (parseFloat(document.getElementById("imagem").className.slice(-1)) + 1);
+    }
+    else{
+        document.getElementById("imagem").src = "../../Img/Manual/Padrinhos_Completo/1.png";
+
+        document.getElementById("imagem").className = "imagem imagem1";
+    }
+
+}
+function voltarImgIframeImg(){
+    if(document.getElementById("imagem").className.slice(-1) > 1){
+        document.getElementById("imagem").src = "../../Img/Manual/Padrinhos_Completo/" + (parseFloat(document.getElementById("imagem").className.slice(-1)) - 1) + ".png";
+
+        document.getElementById("imagem").className = "imagem imagem" + (parseFloat(document.getElementById("imagem").className.slice(-1)) - 1);
+    }
+    else{
+        document.getElementById("imagem").src = "../../Img/Manual/Padrinhos_Completo/9.png";
+
+        document.getElementById("imagem").className = "imagem imagem9";
+    }
+
+}
